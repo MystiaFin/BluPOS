38256535265e97d84257c5606d953fde92fd7f6b
MystiaFin <cytrsx01@gmail.com>
2024-10-04 14:54:01 +0700

    initial commit

---
+++ route.js
@@ -0,0 +1,27 @@
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+};
+
+const routes = {
+    404: "/pages/404.html",
+    "/": "/pages/home/home.html",
+    "/menu": "/pages/menu/menu.html",
+    "/orders": "/pages/order/order.html",
+    "/checkouts": "/pages/checkouts/checkout.html",
+    "/settings": "/pages/settings/settings.html",
+};
+
+const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = html;
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
