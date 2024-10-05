1c15345463589542fdcff81583b7702f9c590578
MystiaFin <cytrsx01@gmail.com>
2024-10-05 21:07:06 +0700

    mobile responsive for home and menu updated

--- pages/order/script.js
+++ pages/order/script.js
@@ -1,9 +1,8 @@
-$(document).ready(function() {
-  
-    // load menu data from JSON
-    $.getJSON('./menus.json', function(data) {
-      data.forEach(function(item) {
-        const cardHtml = `
+$(document).ready(function () {
+  // load menu data from JSON
+  $.getJSON("./menus.json", function (data) {
+    data.forEach(function (item) {
+      const cardHtml = `
           <div class="card">
             <img src="${item.image}" alt="${item.name}">
             <div class="content">
@@ -17,75 +16,77 @@ $(document).ready(function() {
             </div>
           </div>
         `;
-        $('.card-wrapper').append(cardHtml);
-      });
-  
-      // increase quantity
-      $('.card-wrapper').on('click', '.increment', function() {
-        const countElement = $(this).siblings('.count');
-        let count = parseInt(countElement.text());
-        count++;
+      $(".card-wrapper").append(cardHtml);
+    });
+
+    // increase quantity
+    $(".card-wrapper").on("click", ".increment", function () {
+      const countElement = $(this).siblings(".count");
+      let count = parseInt(countElement.text());
+      count++;
+      countElement.text(count);
+      updateOrder($(this).closest(".card").find("h2").text(), count);
+    });
+
+    // decrease quantity
+    $(".card-wrapper").on("click", ".decrement", function () {
+      const countElement = $(this).siblings(".count");
+      let count = parseInt(countElement.text());
+      if (count > 0) {
+        count--;
         countElement.text(count);
-        updateOrder($(this).closest('.card').find('h2').text(), count);
-      });
-  
-      // decrease quantity
-      $('.card-wrapper').on('click', '.decrement', function() {
-        const countElement = $(this).siblings('.count');
-        let count = parseInt(countElement.text());
-        if (count > 0) {
-          count--;
-          countElement.text(count);
-          updateOrder($(this).closest('.card').find('h2').text(), count);
-        }
-      });
-  
-      // updating checkout menu
-      function updateOrder(itemName, count) {
-        const order = $('.orders').find(`.order:contains('${itemName}')`);
-        const itemPrice = data.find(item => item.name === itemName).price;
-  
-        if (count > 0) {
-          if (order.length > 0) {
-            // update existing order
-            order.find('span.count').text(`${count}x`);
-            order.find('span.price').text(`$${(itemPrice * count).toFixed(2)}`);
-          } else {
-            // add new order if order is not exist
-            const newOrderHtml = `
-              <div class="order">
-                <span>${itemName}</span>
-                <span class="count">${count}x</span>
-                <span class="price">$${(itemPrice * count).toFixed(2)}</span>
-              </div>
-            `;
-            $('.orders').prepend(newOrderHtml);
-          }
+        updateOrder($(this).closest(".card").find("h2").text(), count);
+      }
+    });
+
+    // updating checkout menu
+    function updateOrder(itemName, count) {
+      const order = $(".orders").find(`.order:contains('${itemName}')`);
+      const itemPrice = data.find((item) => item.name === itemName).price;
+      if (count > 0) {
+        if (order.length > 0) {
+          // update existing order
+          order.find(".quantity").text(`${count}x`);
+          order.find(".price").text(`$${(itemPrice * count).toFixed(2)}`);
         } else {
-          // remove order if count quantity reach 0
-          order.remove();
+          // add new order if order doesn't exist
+          const newOrderHtml = `
+                  <div class="order">
+                    <div class="order-item name">${itemName}</div>
+                    <div class="order-item quantity">${count}x</div>
+                    <div class="order-item price">$${(
+                      itemPrice * count
+                    ).toFixed(2)}</div>
+                  </div>
+                `;
+          $(".orders").prepend(newOrderHtml);
         }
-  
-        updateTotalBill();
+      } else {
+        // remove order if count quantity reaches 0
+        order.remove();
       }
-  
-      // Calculate and update the total bill
-      function updateTotalBill() {
-        let totalBill = 0; // initiate default state
-        $('.orders .order').each(function() {
-          const itemPrice = parseFloat($(this).find('span.price').text().substring(1));
-          totalBill += itemPrice;
-        });
-        $('.bill-total span:last-child').text(`$${totalBill.toFixed(2)}`);
-      }
-  
-      // handle the pay button click event
-      $('.confirm').click(function() {
-        alert('Checkout successful!');
-        // reset quantities
-        $('.count').text('0');
-        $('.orders .order').remove();
-        $('.bill-total span:last-child').text('$0.00');
+      updateTotalBill();
+    }
+
+    // Calculate and update the total bill
+    function updateTotalBill() {
+      let totalBill = 0;
+      $(".orders .order").each(function () {
+        const itemPrice = parseFloat(
+          $(this).find(".price").text().substring(1)
+        );
+        totalBill += itemPrice;
       });
+      $(".bill-total span:last-child").text(`$${totalBill.toFixed(2)}`);
+    }
+
+    // handle the pay button click event
+    $(".confirm").click(function () {
+      alert("Checkout successful!");
+      // reset quantities
+      $(".count").text("0");
+      $(".orders .order").remove();
+      $(".bill-total span:last-child").text("$0.00");
     });
   });
+});
